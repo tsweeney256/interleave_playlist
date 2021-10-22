950fc3a40725162a43db61c34514d7533a1d2127
Thomas Sweeney <tsweeney256@gmail.com>
2021-10-22 19:27:42 -0400

    type hinting all the things

--- interleave.py
+++ interleave.py
@@ -51,7 +51,7 @@ data = [
 #     ]
 # ]
 
-MARGIN = 10e-6
+MARGIN: float = 10e-6
 
 
 #  wtf I hate python now
@@ -63,28 +63,28 @@ def _divide(a, b) -> float:
 
 
 def _get_every(larger_len: int, smaller_len: int) -> float:
-    ratio = _divide(larger_len, smaller_len + 1) + 1
+    ratio: float = _divide(larger_len, smaller_len + 1) + 1
     return ratio if not isnan(ratio) else larger_len
 
 
 def interleave(a: list[str], b: list[str]) -> list[str]:
     larger, smaller = sorted([a, b], key=lambda ab: len(ab), reverse=True)
-    every = _get_every(len(larger), len(smaller))
-    total_len = len(larger) + len(smaller)
-    result = [""] * total_len
+    every: float = _get_every(len(larger), len(smaller))
+    total_len: int = len(larger) + len(smaller)
+    result: list[str] = [""] * total_len
     for i in range(total_len):
-        use_smaller = 1 > (i + 1 + MARGIN) % every
-        smaller_idx = min(len(smaller), int(i / every))
-        larger_idx = i - smaller_idx
-        arr = smaller if use_smaller else larger
-        idx = smaller_idx if use_smaller else larger_idx
+        use_smaller: bool = 1 > (i + 1 + MARGIN) % every
+        smaller_idx: int = min(len(smaller), int(i / every))
+        larger_idx: int = i - smaller_idx
+        arr: list[str] = smaller if use_smaller else larger
+        idx: int = smaller_idx if use_smaller else larger_idx
         result[i] = arr[idx]
     return result
 
 
 def interleave_all(groups: list[list[str]]) -> list[str]:
     groups.sort(key=lambda f: len(f))
-    result = []
+    result: list[str] = []
     for group in groups:
         result = interleave(result, group)
     return result
