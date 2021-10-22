f3c9b23ea17a65f70ff3f832c051f4ba9b518b42
Thomas Sweeney <tsweeney256@gmail.com>
2021-10-21 23:56:59 -0400

    redundant parens

--- interleave.py
+++ interleave.py
@@ -76,7 +76,7 @@ def interleave(a: list[str], b: list[str]) -> list[str]:
     result = [""] * total_len
     for i in range(total_len):
         which = last_small_idx >= i and 1 > (i + 1 + MARGIN) % every
-        smaller_idx = min(len(smaller), int(i / (every)))
+        smaller_idx = min(len(smaller), int(i / every))
         larger_idx = i - smaller_idx
         result[i] = ab[which][[larger_idx, smaller_idx][which]]
     return result
