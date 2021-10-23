51bd1c5ac3bf877cac964d47b32f890d6395dc8c
Thomas Sweeney <tsweeney256@gmail.com>
2021-10-23 11:58:58 -0400

    create needed files

--- interleave_playlist.py
+++ interleave_playlist.py
@@ -1,4 +1,6 @@
 from interface.PlaylistApplication import PlaylistApplication
+from settings import create_needed_files
 
 if __name__ == "__main__":
+    create_needed_files()
     PlaylistApplication([])
--- settings/__init__.py
+++ settings/__init__.py
@@ -5,6 +5,11 @@ with open('config/settings.yml', 'r') as f:
     _settings: dict[str] = yaml.safe_load(f)
 
 
+def create_needed_files():
+    with open('config/blacklist.txt', 'a'):
+        pass
+
+
 def get_locations() -> list[str]:
     return _settings['locations']
 
