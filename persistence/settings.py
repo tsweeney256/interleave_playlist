ca281976f6c005702215d5240ba015a54f8e22bb
Thomas Sweeney <tsweeney256@gmail.com>
2021-10-28 20:17:05 -0400

    settings file should only be read once

--- persistence/settings.py
+++ persistence/settings.py
@@ -5,6 +5,7 @@ import yaml
 import util
 
 _SETTINGS_FILE = os.path.join(util.SCRIPT_LOC, 'config', 'settings.yml')
+_CACHED_FILE = None
 
 
 def get_font_size() -> int:
@@ -20,8 +21,11 @@ def get_dark_mode() -> bool:
 
 
 def _get_settings():
-    with open(_SETTINGS_FILE, 'r') as f:
-        return yaml.safe_load(f)
+    global _CACHED_FILE
+    if _CACHED_FILE is None:
+        with open(_SETTINGS_FILE, 'r') as f:
+            _CACHED_FILE = yaml.safe_load(f)
+    return _CACHED_FILE
 
 
 def _create_settings_file():
