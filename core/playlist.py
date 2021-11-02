d94c87373c982587e9cbb1fd58c2ee6883d384af
Thomas Sweeney <tsweeney256@gmail.com>
2021-11-01 23:33:03 -0400

    optimize import

--- core/playlist.py
+++ core/playlist.py
@@ -13,7 +13,6 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import re
-from functools import reduce
 from itertools import groupby
 from os import path, listdir
 
