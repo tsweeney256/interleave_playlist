7ce2a38a51abfc4903e68dc25d51acd4f26a1978
Thomas Sweeney <tsweeney256@gmail.com>
2021-12-21 19:16:49 -0500

    write setting to settings.yml if missing

--- persistence/settings.py
+++ persistence/settings.py
@@ -13,6 +13,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
+from pathlib import Path
 
 from ruamel.yaml import YAML
 
@@ -23,36 +24,48 @@ _CACHED_FILE = None
 
 
 def get_font_size() -> int:
-    return _get_settings('font-size', 12)
+    return _get_settings('font-size')
 
 
 def get_play_command() -> str:
-    return _get_settings('play-command', 'mpv')
+    return _get_settings('play-command')
 
 
 def get_dark_mode() -> bool:
-    return _get_settings('dark-mode', False)
+    return _get_settings('dark-mode')
 
 
 def get_max_watched_remembered() -> int:
-    return _get_settings('max-watched-remembered', 100)
+    return _get_settings('max-watched-remembered')
 
 
-def _get_settings(option, default):
+def _get_settings(option):
     global _CACHED_FILE
     if _CACHED_FILE is None:
         with open(_SETTINGS_FILE, 'r') as f:
             yaml = YAML()
             yaml.preserve_quotes = True
             _CACHED_FILE = yaml.load(f)
-    return _CACHED_FILE[option] if option in _CACHED_FILE else default
+            default_settings = _get_default_settings()
+        for key in default_settings.keys():
+            if key not in _CACHED_FILE:
+                _CACHED_FILE[key] = default_settings[key]
+            yaml.dump(_CACHED_FILE, Path(_SETTINGS_FILE))
+
+    return _CACHED_FILE[option]
+
+
+def _get_default_settings() -> dict[str, any]:
+    return {
+        'font-size': 12,
+        'play-command': 'mpv',
+        'dark-mode': False,
+        'max-watched-remembered': 100
+    }
 
 
 def _create_settings_file():
     if not os.path.exists(_SETTINGS_FILE):
         with open(_SETTINGS_FILE, 'w') as f:
-            f.write("""### This settings file is recreated with defaults if deleted ###
-font-size: 12
-play-command: mpv
-dark-mode: false
-max-watched-remembered: 100""")
+            yaml = YAML()
+            yaml.dump(_get_default_settings(), Path(_SETTINGS_FILE))
