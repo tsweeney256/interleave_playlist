eeeb6e24864a5618e1e03a10b6e638410e792b6a
Thomas Sweeney <tsweeney256@gmail.com>
2022-08-16 18:46:29 -0400

    Update copyright

--- persistence/input_.py
+++ persistence/input_.py
@@ -1,5 +1,5 @@
 #    Interleave Playlist
-#    Copyright (C) 2021 Thomas Sweeney
+#    Copyright (C) 2021-2022 Thomas Sweeney
 #    This file is part of Interleave Playlist.
 #    Interleave Playlist is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
