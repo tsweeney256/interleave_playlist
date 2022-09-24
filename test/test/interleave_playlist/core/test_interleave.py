33a22063ba9a604bcfcbde8f32e60948492db34d
Thomas Sweeney <tsweeney256@gmail.com>
2022-09-24 11:09:02 -0400

    make test data manipulation more generic

--- test/test/interleave_playlist/core/test_interleave.py
+++ test/test/interleave_playlist/core/test_interleave.py
@@ -20,8 +20,12 @@ from interleave_playlist.core.interleave import interleave, interleave_all
 InterleaveParameterSet = ParameterSet[list[str], list[str], list[str]]
 
 
-def transform_testdata_definition(a, b, expected, k) -> InterleaveParameterSet:
-    return pytest.param(["w"] * a, ["U"] * b, [c for c in expected], id=f'{k=} {(a, b, expected)}')
+def transform_testdata_definition(input_: list[int], expected: str, k: int, values: list[str]) \
+        -> InterleaveParameterSet:
+    transformed_input: list[list[str]] = []
+    for idx, size in enumerate(input_):
+        transformed_input.append([values[idx]] * size)
+    return pytest.param(transformed_input, [c for c in expected], id=f'{k=} {(input_, expected)}')
 
 
 def reverse_testdata_arguments(a, b, expected) -> tuple[int, int, str]:
@@ -33,93 +37,100 @@ t = transform_testdata_definition
 r = reverse_testdata_arguments
 
 interleave_testdata_definition = [
-    (0, 0, ""),
-    (0, 1, "U"),
-    (0, 2, "UU"),
-    (0, 3, "UUU"),
-    (0, 4, "UUUU"),
-    (0, 5, "UUUUU"),
-    (0, 6, "UUUUUU"),
-    (0, 7, "UUUUUUU"),
-    (0, 8, "UUUUUUUU"),
-    (0, 9, "UUUUUUUUU"),
-    (0, 10, "UUUUUUUUUU"),
-    (1, 1, "wU"),
-    (1, 2, "UwU"),
-    (1, 3, "UwUU"),
-    (1, 4, "UUwUU"),
-    (1, 5, "UUwUUU"),
-    (1, 6, "UUUwUUU"),
-    (1, 7, "UUUwUUUU"),
-    (1, 8, "UUUUwUUUU"),
-    (1, 9, "UUUUwUUUUU"),
-    (1, 10, "UUUUUwUUUUU"),
-    (2, 2, "wUwU"),
-    (2, 3, "UwUwU"),
-    (2, 4, "UwUwUU"),
-    (2, 5, "UwUUwUU"),
-    (2, 6, "UUwUUwUU"),
-    (2, 7, "UUwUUwUUU"),
-    (2, 8, "UUwUUUwUUU"),
-    (2, 9, "UUUwUUUwUUU"),
-    (2, 10, "UUUwUUUwUUUU"),
-    (3, 3, "wUwUwU"),
-    (3, 4, "UwUwUwU"),
-    (3, 5, "UwUwUwUU"),
-    (3, 6, "UwUUwUwUU"),
-    (3, 7, "UwUUwUUwUU"),
-    (3, 8, "UUwUUwUUwUU"),
-    (3, 9, "UUwUUwUUwUUU"),
-    (3, 10, "UUwUUUwUUwUUU"),
-    (4, 4, "wUwUwUwU"),
-    (4, 5, "UwUwUwUwU"),
-    (4, 6, "UwUwUwUwUU"),
-    (4, 7, "UwUwUUwUwUU"),
-    (4, 8, "UwUUwUwUUwUU"),
-    (4, 9, "UwUUwUUwUUwUU"),
-    (4, 10, "UUwUUwUUwUUwUU"),
-    (5, 5, "wUwUwUwUwU"),
-    (5, 6, "UwUwUwUwUwU"),
-    (5, 7, "UwUwUwUwUwUU"),
-    (5, 8, "UwUwUUwUwUwUU"),
-    (5, 9, "UwUUwUwUUwUwUU"),
-    (5, 10, "UwUUwUUwUwUUwUU"),
-    (6, 6, "wUwUwUwUwUwU"),
-    (6, 7, "UwUwUwUwUwUwU"),
-    (6, 8, "UwUwUwUwUwUwUU"),
-    (6, 9, "UwUwUwUUwUwUwUU"),
-    (6, 10, "UwUwUUwUwUUwUwUU"),
-    (7, 7, "wUwUwUwUwUwUwU"),
-    (7, 8, "UwUwUwUwUwUwUwU"),
-    (7, 9, "UwUwUwUwUwUwUwUU"),
-    (7, 10, "UwUwUwUUwUwUwUwUU"),
-    (8, 8, "wUwUwUwUwUwUwUwU"),
-    (8, 9, "UwUwUwUwUwUwUwUwU"),
-    (8, 10, "UwUwUwUwUwUwUwUwUU"),
-    (9, 9, "wUwUwUwUwUwUwUwUwU"),
-    (10, 10, "wUwUwUwUwUwUwUwUwUwU"),
+    ([0, 0], ""),
+    ([0, 1], "U"),
+    ([0, 2], "UU"),
+    ([0, 3], "UUU"),
+    ([0, 4], "UUUU"),
+    ([0, 5], "UUUUU"),
+    ([0, 6], "UUUUUU"),
+    ([0, 7], "UUUUUUU"),
+    ([0, 8], "UUUUUUUU"),
+    ([0, 9], "UUUUUUUUU"),
+    ([0, 10], "UUUUUUUUUU"),
+    ([1, 1], "wU"),
+    ([1, 2], "UwU"),
+    ([1, 3], "UwUU"),
+    ([1, 4], "UUwUU"),
+    ([1, 5], "UUwUUU"),
+    ([1, 6], "UUUwUUU"),
+    ([1, 7], "UUUwUUUU"),
+    ([1, 8], "UUUUwUUUU"),
+    ([1, 9], "UUUUwUUUUU"),
+    ([1, 10], "UUUUUwUUUUU"),
+    ([2, 2], "wUwU"),
+    ([2, 3], "UwUwU"),
+    ([2, 4], "UwUwUU"),
+    ([2, 5], "UwUUwUU"),
+    ([2, 6], "UUwUUwUU"),
+    ([2, 7], "UUwUUwUUU"),
+    ([2, 8], "UUwUUUwUUU"),
+    ([2, 9], "UUUwUUUwUUU"),
+    ([2, 10], "UUUwUUUwUUUU"),
+    ([3, 3], "wUwUwU"),
+    ([3, 4], "UwUwUwU"),
+    ([3, 5], "UwUwUwUU"),
+    ([3, 6], "UwUUwUwUU"),
+    ([3, 7], "UwUUwUUwUU"),
+    ([3, 8], "UUwUUwUUwUU"),
+    ([3, 9], "UUwUUwUUwUUU"),
+    ([3, 10], "UUwUUUwUUwUUU"),
+    ([4, 4], "wUwUwUwU"),
+    ([4, 5], "UwUwUwUwU"),
+    ([4, 6], "UwUwUwUwUU"),
+    ([4, 7], "UwUwUUwUwUU"),
+    ([4, 8], "UwUUwUwUUwUU"),
+    ([4, 9], "UwUUwUUwUUwUU"),
+    ([4, 10], "UUwUUwUUwUUwUU"),
+    ([5, 5], "wUwUwUwUwU"),
+    ([5, 6], "UwUwUwUwUwU"),
+    ([5, 7], "UwUwUwUwUwUU"),
+    ([5, 8], "UwUwUUwUwUwUU"),
+    ([5, 9], "UwUUwUwUUwUwUU"),
+    ([5, 10], "UwUUwUUwUwUUwUU"),
+    ([6, 6], "wUwUwUwUwUwU"),
+    ([6, 7], "UwUwUwUwUwUwU"),
+    ([6, 8], "UwUwUwUwUwUwUU"),
+    ([6, 9], "UwUwUwUUwUwUwUU"),
+    ([6, 10], "UwUwUUwUwUUwUwUU"),
+    ([7, 7], "wUwUwUwUwUwUwU"),
+    ([7, 8], "UwUwUwUwUwUwUwU"),
+    ([7, 9], "UwUwUwUwUwUwUwUU"),
+    ([7, 10], "UwUwUwUUwUwUwUwUU"),
+    ([8, 8], "wUwUwUwUwUwUwUwU"),
+    ([8, 9], "UwUwUwUwUwUwUwUwU"),
+    ([8, 10], "UwUwUwUwUwUwUwUwUU"),
+    ([9, 9], "wUwUwUwUwUwUwUwUwU"),
+    ([10, 10], "wUwUwUwUwUwUwUwUwUwU"),
+]
+
+interleave_all_test_data_definition = [
+    ([], ""),
+    ([0], ""),
+    ([0, 0], ""),
+    ([0, 0, 0], ""),
+    ([0, 0, 0, 0], "")
 ]
 
 interleave_testdata: list[InterleaveParameterSet] = []
 i = 0
 for d in interleave_testdata_definition:
-    interleave_testdata.append(t(*d, i))
+    interleave_testdata.append(t(*d, k=i, values=['w', 'U']))
     i += 1
-    if d[0] != d[1]:
-        interleave_testdata.append(t(*r(*d), i))
-        i += 1
-
-interleave_all_from_interleave_testdata: list[tuple[list[list[str]], str]] \
-    = [([x.values[0], x.values[1]], x.values[2]) for x in interleave_testdata]  # type: ignore
+    if d[0][0] != d[0][1]:
+        reversed_ = r(d[0][0], d[0][1], d[1])
+        interleave_testdata.append(t([reversed_[0], reversed_[1]],
+                                     reversed_[2], k=i, values=['w', 'U']))
 
 
-@pytest.mark.parametrize("a,b,expected", interleave_testdata)
-def test_interleave(a, b, expected):
+@pytest.mark.parametrize("input_,expected", interleave_testdata)
+def test_interleave(input_, expected):
+    a, b = input_
     actual = interleave(a, b)
     assert(actual == expected)
 
 
-@pytest.mark.parametrize("groups,expected", interleave_all_from_interleave_testdata)
+@pytest.mark.parametrize("groups,expected", interleave_testdata)
 def test_interleave_all_acts_same_as_interleave_with_two_inputs(groups, expected):
     actual = interleave_all(groups)
     assert(actual == expected)
