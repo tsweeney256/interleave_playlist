4a31af8ac69310fdfb5431820356a29345db0ee0
Thomas Sweeney <tsweeney256@gmail.com>
2022-10-03 19:55:06 -0400

    test playlist wishlist

---
+++ test/tests/interleave_playlist/core/test_playlist.py
@@ -0,0 +1,219 @@
+#    Interleave Playlist
+#    Copyright (C) 2022 Thomas Sweeney
+#    This file is part of Interleave Playlist.
+#    Interleave Playlist is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#    Interleave Playlist is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import pytest
+
+
+@pytest.mark.skip
+def test_get_playlist_with_no_locations():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_one_location():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_many_locations():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_many_duplicate_locations():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_one_location_one_regex_group():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_one_location_man_regex_groups():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_many_locations_one_regex_group_each():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_many_locations_many_regex_groups_each():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_one_location_using_regex_other_no_regex():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_many_locations_different_priorities():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_one_location_many_regex_groups_different_priorities():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_many_location_many_regex_groups_different_priorities():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_search_filter_matching_exact():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_search_filter_matching_subset():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_search_filter_matching_subset_case_insensitive():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_whitelist_exact():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_whitelist_subset():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_whitelist_subset_case_insensitive():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_blacklist_exact():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_blacklist_subset():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_blacklist_subset_case_insensitive():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_exclude_directories_setting_on():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_not_exclude_directories_setting_off():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_blacklist_and_whitelist_working_together():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_blacklist_and_whitelist_contradicting():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_regex_and_whitelist():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_regex_and_blacklist():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_regex_and_whitelist_and_blacklist():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_one_loc_timed_for_future():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_one_loc_timed():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_many_loc_timed():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_many_amount():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_starting_at_episode():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_one_watched():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_many_watched():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_watched_not_in_list():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_timed_and_watched():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_with_least_recently_watched_bias():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_using_cache_without_existing_yet():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_using_cache_with_existing_after_update():
+    pass
+
+
+@pytest.mark.skip
+def test_get_playlist_using_cache_after_refreshing_cache_after_update():
+    pass
